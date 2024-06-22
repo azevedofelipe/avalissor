44de0774571c6205b1f4e0e7ed6ea09647136003
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-06-22 03:23:48 -0300

    Registered Models to admin

--- professores/admin.py
+++ professores/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comentario,Professor,Curso,Faculdade
 
 # Register your models here.
+admin.site.register(Comentario)
+admin.site.register(Professor)
+admin.site.register(Curso)
+admin.site.register(Faculdade)
