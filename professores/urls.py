06931b3191e6852fea2ed3790942b364058e4dcb
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-06-24 01:35:10 -0300

    Commenting out APIs for now, creating everything in html directly

--- professores/urls.py
+++ professores/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'professor'
 
 urlpatterns = [
-    path('professor/',views.ProfessorList.as_view()),                     # Lista de professores
-    path('professor/<int:pk>/',views.ProfessorDetalhes.as_view()),        # Detalhes de professor, pk de professor especifico
-    path('comentarios/',views.ComentarioCriar.as_view()),        # Criar Comentarios sobre professor
-    path('comentarios/<int:pk>',views.ComentarioDetalhes.as_view()), # Detalhes de comentario, editar se for dono
-    path('likes/',views.LikeCriar.as_view()),    # Criar Likes para comentarios
-    path('likes/<int:pk>',views.LikeDetalhes.as_view()) # Alterar like
+#    path('professor/',views.ProfessorList.as_view()),                     # Lista de professores
+    path('professores/',views.professor_list,name='professor_list'),                     # Lista de professores
+#    path('professor/<int:pk>/',views.ProfessorDetalhes.as_view()),        # Detalhes de professor, pk de professor especifico
+#    path('comentarios/',views.ComentarioCriar.as_view()),        # Criar Comentarios sobre professor
+#    path('comentarios/<int:pk>',views.ComentarioDetalhes.as_view()), # Detalhes de comentario, editar se for dono
+#    path('likes/',views.LikeCriar.as_view()),    # Criar Likes para comentarios
+#    path('likes/<int:pk>',views.LikeDetalhes.as_view()) # Alterar like
 
 ]
--- professores/views.py
+++ professores/views.py
@@ -7,53 +7,58 @@ from .serializers import ProfessorSerializer,ComentarioSerializer, LikesComentar
 from django_filters import rest_framework as filters 
 
 # API para listar todos os professores
-class ProfessorList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly,]
-
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = {
-        'nome' : ['contains'],
-        'faculdade' : ['in'],
-        'curso' : ['in']
-    }
-    serializer_class = ProfessorSerializer
-    queryset = Professor.objects.all().distinct()
+#class ProfessorList(generics.ListCreateAPIView):
+#    permission_classes = [IsAuthenticatedOrReadOnly,]
+#
+#    filter_backends = (filters.DjangoFilterBackend,)
+#    filterset_fields = {
+#        'nome' : ['contains'],
+#        'faculdade' : ['in'],
+#        'curso' : ['in']
+#    }
+#    serializer_class = ProfessorSerializer
+#    queryset = Professor.objects.all().distinct()
     
+def professor_list(request):
+    professores = Professor.objects.all()
+    return render(request,'professors.html',{'professores':professores})
+
+
 # API para RUD de professores C[RUD]
-class ProfessorDetalhes(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [isOwner,]
-
-    serializer_class = ProfessorSerializer
-    queryset = Professor.objects.all()
-
-
-# API para criar comentario
-class ComentarioCriar(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    serializer_class = ComentarioSerializer
-    queryset = Comentario.objects.all()
-
-
-# API para ver detalhes de comentario
-class ComentarioDetalhes(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (isOwner,)
-
-    serializer_class = ComentarioSerializer
-    queryset = Comentario.objects.all()
-    
-
-# API para likes para comentarios
-class LikeCriar(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    
-    serializer_class = LikesComentarioSerializer
-    queryset = LikesComentarios.objects.all()
-
-
-# API para alterar like
-class LikeDetalhes(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (isOwner,)
-
-    serializer_class = LikesComentarioSerializer
-    queryset = LikesComentarios.objects.all()
+#class ProfessorDetalhes(generics.RetrieveUpdateDestroyAPIView):
+#    permission_classes = [isOwner,]
+#
+#    serializer_class = ProfessorSerializer
+#    queryset = Professor.objects.all()
+#
+#
+## API para criar comentario
+#class ComentarioCriar(generics.ListCreateAPIView):
+#    permission_classes = (IsAuthenticatedOrReadOnly,)
+#
+#    serializer_class = ComentarioSerializer
+#    queryset = Comentario.objects.all()
+#
+#
+## API para ver detalhes de comentario
+#class ComentarioDetalhes(generics.RetrieveUpdateDestroyAPIView):
+#    permission_classes = (isOwner,)
+#
+#    serializer_class = ComentarioSerializer
+#    queryset = Comentario.objects.all()
+#    
+#
+## API para likes para comentarios
+#class LikeCriar(generics.ListCreateAPIView):
+#    permission_classes = (IsAuthenticatedOrReadOnly,)
+#    
+#    serializer_class = LikesComentarioSerializer
+#    queryset = LikesComentarios.objects.all()
+#
+#
+## API para alterar like
+#class LikeDetalhes(generics.RetrieveUpdateDestroyAPIView):
+#    permission_classes = (isOwner,)
+#
+#    serializer_class = LikesComentarioSerializer
+#    queryset = LikesComentarios.objects.all()
