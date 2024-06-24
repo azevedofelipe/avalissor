5f61fb0132c9847afddfaa2c57a20d31e878b187
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-06-23 22:25:32 -0300

    Fixed urls

--- avalissor/urls.py
+++ avalissor/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/professor/', include('professores.urls'))
+    path('api/', include('professores.urls'))
 ]
--- professores/urls.py
+++ professores/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'professor'
 
 urlpatterns = [
-    path('',views.ProfessorList.as_view()),                     # Lista de professores
-    path('<int:pk>/',views.ProfessorDetalhes.as_view()),        # Detalhes de professor, pk de professor especifico
+    path('professor',views.ProfessorList.as_view()),                     # Lista de professores
+    path('professor/<int:pk>/',views.ProfessorDetalhes.as_view()),        # Detalhes de professor, pk de professor especifico
     path('comentarios',views.ComentarioCriar.as_view()),        # Criar Comentarios sobre professor
     path('comentarios/<int:pk>',views.ComentarioDetalhes.as_view()), # Detalhes de comentario, editar se for dono
     path('likes',views.LikeCriar.as_view()),    # Criar Likes para comentarios
